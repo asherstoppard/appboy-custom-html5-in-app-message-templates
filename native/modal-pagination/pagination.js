1a407958681d8a5f85dedeaa6f4a409fedf04286
Asher Stoppard <asher@asherstoppard.com>
2020-04-01 16:45:46 +0100

    First pass

--- native/modal-pagination/pagination.js
+++ native/modal-pagination/pagination.js
@@ -6,31 +6,34 @@ $(document).ready(function(){
   $('.slick-container').slick({
     infinite: false,
     edgeFriction: 0,
-    prevArrow: false,
+    prevArrow: $('.button-left'),
     nextArrow: $('.button-right'),
-    dots: true,
-    customPaging : function() {
-      return '<span class="slick-dot"></span>'
-    }
+    dots: false
   });
 
-  // Initially hide the "GOT IT" button until you reach the last slide
-  $('.button-center').hide();
-  // This event listener changes the footer buttons.  When the modal reaches the last slide, 
+  var prevButton = $('.js-button-prev');
+  var nextButton = $('.js-button-next');
+  var slideNumber = $('.js-slide')
+
+  prevButton.hide();
+
+  // This event listener changes the footer buttons.  When the modal reaches the last slide,
   // it hides the "SKIP" and "NEXT" buttons and displays the "GOT IT!" button
   $('.slick-container').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-    var lastSlide = slick.slideCount-1
-    var secondToLastSlide = slick.slideCount-2
-    if (slick.slideCount >= 2) {
-      if (currentSlide === secondToLastSlide && nextSlide == lastSlide) {
-        $('.button-left').hide();
-        $('.button-right').hide();
-        $('.button-center').show();
-      } else if (currentSlide === lastSlide && nextSlide == secondToLastSlide) {
-        $('.button-left').show();
-        $('.button-right').show();
-        $('.button-center').hide();
-      };
-    };
+    var lastSlide = slick.slideCount - 1;
+    slideNumber.html(nextSlide + 1)
+    if (nextSlide === 0) {
+      prevButton.hide();
+      nextButton.show();
+      return
+    }
+    if (nextSlide === lastSlide) {
+      prevButton.show();
+      nextButton.hide();
+      return
+    }
+    prevButton.show();
+    nextButton.show();
+    return
   });
 });
